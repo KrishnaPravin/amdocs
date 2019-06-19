6e2b5f527f2f765bf4bfd187adc71a5813361756
Krishna Pravin <krishna.pravin@gyanmatrix.com>
2019-06-19 18:57:26 +0530

    first commit

---
+++ single-entry-single-exit/src/main/java/com/amdocs/CleanCodePractices/ExamResult.java
@@ -0,0 +1,10 @@
+package com.amdocs.CleanCodePractices;
+
+public class ExamResult {
+	public final static String FAIL = "Fail";
+	public final static String PASS = "Pass";
+	public final static String FIRST_CLASS = "First Class";
+	public final static String DISTINCTION = "Distinction";
+	public final static String LESS_THAN_ZERO_ERROR = "Mark cannot be less than 0";
+	public final static String GREATER_THAN_HUNDRED_ERROR = "Mark cannot be greater than 100";
+}
